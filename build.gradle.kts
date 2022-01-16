f8bf4b904df3464344a24d467147c5798ce28820
ColdRain-Moro <rain@asgard.hk>
2022-01-16 15:32:06 +0800

    [feature] update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,22 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.5.10"
     java
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "kim.bifrost.rain.retrofit"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://gitee.com/coldrain-moro/maven/raw/master/repo") }
 }
 
 dependencies {
+    implementation("ink.ptms.tiphareth:tiphareth:1.4.0")
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("com.google.code.gson:gson:2.8.9")
@@ -20,6 +25,35 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
+tasks.withType<ShadowJar> {
+    dependencies {
+        include(dependency("com.google.code.gson:gson:2.8.9"))
+        include(dependency("com.squareup.okhttp3:okhttp:4.9.3"))
+    }
+}
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "retrofit"
+            groupId = "kim.bifrost.rain"
+            version = project.version.toString()
+            println("> version $version")
+            file("$buildDir/libs").listFiles()?.forEach { file ->
+                if (file.extension == "jar") {
+                    artifact(file) {
+                        classifier = file.nameWithoutExtension.substring(0, file.nameWithoutExtension.length - project.version.toString().length - 1)
+                        println("> module $classifier (${file.name})")
+                    }
+                }
+            }
+        }
+    }
 }
